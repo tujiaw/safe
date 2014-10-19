207ec561271ed5f0dc9219c650ede91053b32192
tujiaw <tujiaw@163.com>
2014-10-19 17:20:43 +0800

    ts翻译，lrease to qm

--- Resources/safe_ch.ts
+++ Resources/safe_ch.ts
@@ -2,84 +2,283 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="zh_CN">
 <context>
+    <name>ChaShaWidget</name>
+    <message>
+        <location filename="../ChaShaWidget.cpp" line="35"/>
+        <source>the last scanning</source>
+        <translation>上次扫描是</translation>
+    </message>
+    <message>
+        <location filename="../ChaShaWidget.cpp" line="35"/>
+        <source>no threat found</source>
+        <translation>天前，扫描未发现电脑存在任务威胁。</translation>
+    </message>
+    <message>
+        <location filename="../ChaShaWidget.cpp" line="43"/>
+        <source>regular scanning</source>
+        <translation>定期扫描，可以及时发现威胁并进行处理。</translation>
+    </message>
+    <message>
+        <location filename="../ChaShaWidget.cpp" line="46"/>
+        <source>fast scan</source>
+        <translation>快速扫描</translation>
+    </message>
+    <message>
+        <location filename="../ChaShaWidget.cpp" line="47"/>
+        <source>full scan</source>
+        <translation>全盘扫描</translation>
+    </message>
+    <message>
+        <location filename="../ChaShaWidget.cpp" line="48"/>
+        <source>custom scan</source>
+        <translation>自定义扫描</translation>
+    </message>
+</context>
+<context>
+    <name>MainMenu</name>
+    <message>
+        <location filename="../MainMenu.cpp" line="8"/>
+        <source>setting</source>
+        <translation>设置</translation>
+    </message>
+    <message>
+        <location filename="../MainMenu.cpp" line="9"/>
+        <source>new character</source>
+        <translation>新版特性</translation>
+    </message>
+    <message>
+        <location filename="../MainMenu.cpp" line="10"/>
+        <source>check update</source>
+        <translation>检查更新</translation>
+    </message>
+    <message>
+        <location filename="../MainMenu.cpp" line="11"/>
+        <source>to enterprise</source>
+        <translation>切换到企业版</translation>
+    </message>
+    <message>
+        <location filename="../MainMenu.cpp" line="12"/>
+        <source>online help</source>
+        <translation>在线帮助</translation>
+    </message>
+    <message>
+        <location filename="../MainMenu.cpp" line="13"/>
+        <source>forum help</source>
+        <translation>论坛求助</translation>
+    </message>
+    <message>
+        <location filename="../MainMenu.cpp" line="14"/>
+        <source>360 website</source>
+        <translation>360网站</translation>
+    </message>
+    <message>
+        <location filename="../MainMenu.cpp" line="15"/>
+        <source>privacy protection</source>
+        <translation>隐私保护</translation>
+    </message>
+    <message>
+        <location filename="../MainMenu.cpp" line="16"/>
+        <source>about us</source>
+        <translation>关于我们</translation>
+    </message>
+</context>
+<context>
+    <name>TiJianWidget</name>
+    <message>
+        <location filename="../TiJianWidget.cpp" line="60"/>
+        <source>suggest</source>
+        <translation>上次体检良好，请继续保持每天体检的习惯</translation>
+    </message>
+    <message>
+        <location filename="../TiJianWidget.cpp" line="68"/>
+        <source>system safe</source>
+        <translation>上次体检时间是1分钟前，还有2个优化项可以优化</translation>
+    </message>
+    <message>
+        <location filename="../TiJianWidget.cpp" line="75"/>
+        <source>power</source>
+        <translation>立即体检</translation>
+    </message>
+    <message>
+        <location filename="../TiJianWidget.cpp" line="122"/>
+        <source>login home</source>
+        <translation>登录我的360账号</translation>
+    </message>
+    <message>
+        <location filename="../TiJianWidget.cpp" line="134"/>
+        <source>show beautifull icon</source>
+        <translation>开启炫动卫士图标</translation>
+    </message>
+    <message>
+        <location filename="../TiJianWidget.cpp" line="137"/>
+        <source>register</source>
+        <translation>10秒免费注册</translation>
+    </message>
+    <message>
+        <location filename="../TiJianWidget.cpp" line="142"/>
+        <source>privilege power</source>
+        <translation>专享特权：</translation>
+    </message>
+    <message>
+        <location filename="../TiJianWidget.cpp" line="202"/>
+        <source>fireproof</source>
+        <translation>木马防火墙</translation>
+    </message>
+    <message>
+        <location filename="../TiJianWidget.cpp" line="203"/>
+        <source>triggerman</source>
+        <translation>360保镖</translation>
+    </message>
+    <message>
+        <location filename="../TiJianWidget.cpp" line="204"/>
+        <source>net shop</source>
+        <translation>网购先赔</translation>
+    </message>
+    <message>
+        <location filename="../TiJianWidget.cpp" line="216"/>
+        <source>function</source>
+        <translation>功能大全</translation>
+    </message>
+    <message>
+        <location filename="../TiJianWidget.cpp" line="223"/>
+        <source>more</source>
+        <translation>更多</translation>
+    </message>
+    <message>
+        <location filename="../TiJianWidget.cpp" line="229"/>
+        <source>recovery</source>
+        <translation>文件恢复</translation>
+    </message>
+    <message>
+        <location filename="../TiJianWidget.cpp" line="230"/>
+        <source>mobile</source>
+        <translation>手机助手</translation>
+    </message>
+    <message>
+        <location filename="../TiJianWidget.cpp" line="231"/>
+        <source>game box</source>
+        <translation>游戏盒子</translation>
+    </message>
+    <message>
+        <location filename="../TiJianWidget.cpp" line="232"/>
+        <source>desktop</source>
+        <translation>安全桌面</translation>
+    </message>
+    <message>
+        <location filename="../TiJianWidget.cpp" line="233"/>
+        <source>net repair</source>
+        <translation>断网急救箱</translation>
+    </message>
+    <message>
+        <location filename="../TiJianWidget.cpp" line="234"/>
+        <source>auto run</source>
+        <translation>开机加速</translation>
+    </message>
+    <message>
+        <location filename="../TiJianWidget.cpp" line="235"/>
+        <source>net speed</source>
+        <translation>宽带测速器</translation>
+    </message>
+    <message>
+        <location filename="../TiJianWidget.cpp" line="236"/>
+        <source>net pretext</source>
+        <translation>360网盾</translation>
+    </message>
+    <message>
+        <location filename="../TiJianWidget.cpp" line="237"/>
+        <source>first add</source>
+        <translation>系统急救箱</translation>
+    </message>
+    <message>
+        <location filename="../TiJianWidget.cpp" line="280"/>
+        <source>connect success</source>
+        <translation>成功连接至云安全中心</translation>
+    </message>
+    <message>
+        <location filename="../TiJianWidget.cpp" line="283"/>
+        <source>version</source>
+        <translation>9.1.0.2001</translation>
+    </message>
+</context>
+<context>
     <name>TitleWidget</name>
     <message>
-        <location filename="../TitleWidget.cpp" line="124"/>
+        <location filename="../TitleWidget.cpp" line="123"/>
         <source>title</source>
         <translation>360安全卫士</translation>
     </message>
     <message>
-        <location filename="../TitleWidget.cpp" line="125"/>
+        <location filename="../TitleWidget.cpp" line="124"/>
         <source>change  skin</source>
         <translation>换肤</translation>
     </message>
     <message>
-        <location filename="../TitleWidget.cpp" line="126"/>
+        <location filename="../TitleWidget.cpp" line="125"/>
         <source>main menu</source>
         <translation>主菜单</translation>
     </message>
     <message>
-        <location filename="../TitleWidget.cpp" line="127"/>
+        <location filename="../TitleWidget.cpp" line="126"/>
         <source>minimize</source>
         <translation>最小化</translation>
     </message>
     <message>
-        <location filename="../TitleWidget.cpp" line="128"/>
+        <location filename="../TitleWidget.cpp" line="127"/>
         <source>feedback</source>
         <translation>反馈意见</translation>
     </message>
     <message>
-        <location filename="../TitleWidget.cpp" line="129"/>
+        <location filename="../TitleWidget.cpp" line="128"/>
         <source>close</source>
         <translation>关闭</translation>
     </message>
     <message>
-        <location filename="../TitleWidget.cpp" line="132"/>
+        <location filename="../TitleWidget.cpp" line="131"/>
         <source>power</source>
         <translation>电脑体检</translation>
     </message>
     <message>
-        <location filename="../TitleWidget.cpp" line="133"/>
+        <location filename="../TitleWidget.cpp" line="132"/>
         <source>mummy</source>
         <translation>木马查杀</translation>
     </message>
     <message>
-        <location filename="../TitleWidget.cpp" line="134"/>
+        <location filename="../TitleWidget.cpp" line="133"/>
         <source>repair</source>
         <translation>系统修复</translation>
     </message>
     <message>
-        <location filename="../TitleWidget.cpp" line="135"/>
+        <location filename="../TitleWidget.cpp" line="134"/>
         <source>clear</source>
         <translation>电脑清理</translation>
     </message>
     <message>
-        <location filename="../TitleWidget.cpp" line="136"/>
+        <location filename="../TitleWidget.cpp" line="135"/>
         <source>optimize</source>
         <translation>优化加速</translation>
     </message>
     <message>
-        <location filename="../TitleWidget.cpp" line="137"/>
+        <location filename="../TitleWidget.cpp" line="136"/>
         <source>expert</source>
         <translation>电脑专家</translation>
     </message>
     <message>
-        <location filename="../TitleWidget.cpp" line="138"/>
+        <location filename="../TitleWidget.cpp" line="137"/>
         <source>hole</source>
         <translation>漏洞修复</translation>
     </message>
     <message>
-        <location filename="../TitleWidget.cpp" line="139"/>
+        <location filename="../TitleWidget.cpp" line="138"/>
         <source>software</source>
         <translation>软件管家</translation>
     </message>
     <message>
-        <location filename="../TitleWidget.cpp" line="142"/>
+        <location filename="../TitleWidget.cpp" line="141"/>
         <source>360</source>
         <translation>360</translation>
     </message>
     <message>
-        <location filename="../TitleWidget.cpp" line="143"/>
+        <location filename="../TitleWidget.cpp" line="142"/>
         <source>safe</source>
         <translation>安全卫士</translation>
     </message>
